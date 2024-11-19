15705c6caafd102842c3c1b87a875969170b54bf
Dmitry1987 <dmitry@example.com>
2024-11-19 20:57:47 +0100

    add autoscaling group and adjust few things

--- challenge-3/terraform/main.tf
+++ challenge-3/terraform/main.tf
@@ -46,6 +46,113 @@ module "alb" {
   ]
 }
 
+
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+
+  # Autoscaling group
+  name = "${var.project}-${var.environment}-asg"
+
+  min_size                  = 0
+  max_size                  = 1
+  # the desired should always be set at null, otherwise it will try to adjust the existing groups every time on apply,
+  # I'm surprised the official example shows a '1'
+  desired_capacity          = null
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  # which subnets to use 
+  vpc_zone_identifier       = module.vpc.private_subnet_ids
+
+  launch_template_name        = "${var.project}-${var.environment}-lt"
+  launch_template_description = "Launch template default"
+  update_default_version      = true
+
+  # probably our specific ones from the Pacler baked images, or the latest ubuntu. 
+  # here using the latest Ubuntu from the auto finder by filters, in ami-select.tf 
+  image_id          = data.aws_ami.ubuntu.id
+  instance_type     = var.instance_size
+  ebs_optimized     = true
+  enable_monitoring = true
+  # I don't like the AZ rebalance terminating instances and getting into infinite loops when coupled with k8s autoscaler that fights back :D 
+  # also if nodes run at 90-100% routinely for a long time, it's worth to suspend the 'ReplaceUnhealthy' since the internal ec2 health check will always be unhappy about it.
+  suspended_processes = ["AZRebalance"]
+
+  # no need to add IAM roles, since in the challenge we're not asked to access the S3 explicitly, :D 
+
+  # default disks
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 30
+        volume_type           = "gp3"
+      }
+    }
+  ]
+
+  # These will not always work, depends on instance type, c7 for example is already single thread per physical core and can't be adjusted, etc'
+  cpu_options = {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+  # spots in order to not waste money when testing this terraform! 
+  instance_market_options = {
+    market_type = "spot"
+    spot_options = {
+      block_duration_minutes = 10
+    }
+  }
+
+  metadata_options = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  tag_specifications = [
+    {
+      resource_type = "instance"
+      tags          = {
+        ASG = "${var.project}-${var.environment}-asg"
+        Environment = var.environment
+        Project     = var.project
+        Owner = "DevOps team"
+      }
+    },
+    {
+      resource_type = "volume"
+      tags          = {
+        ASG = "${var.project}-${var.environment}-asg"
+        Environment = var.environment
+        Project     = var.project
+        Owner = "DevOps team"
+      }
+    },
+    {
+      resource_type = "spot-instances-request"
+      tags          = {
+        ASG = "${var.project}-${var.environment}-asg"
+        Environment = var.environment
+        Project     = var.project
+        Owner = "DevOps team"
+      }
+    }
+  ]
+
+  tags = {
+    ASG = "${var.project}-${var.environment}-asg"
+    Environment = var.environment
+    Project     = var.project
+    Owner = "DevOps team"
+  }
+}
+
+
 # RDS will use existing module 
 module "postgres" {
   source  = "terraform-aws-modules/rds-aurora/aws"
