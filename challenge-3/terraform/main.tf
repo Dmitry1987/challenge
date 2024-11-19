3a5ceee7e315f42dc4aa7c228fe143844d5fd59a
Dmitry1987 <dmitry@example.com>
2024-11-19 15:11:48 +0100

    add alb

--- challenge-3/terraform/main.tf
+++ challenge-3/terraform/main.tf
@@ -10,3 +10,17 @@ module "vpc" {
   environment = var.environment
   project_name  = var.project
 }
+
+# balancer for RDS database
+module "alb" {
+  source = "./modules/vpc"
+  
+  internal = true 
+  aws_region  = var.aws_region
+  vpc_cidr    = var.vpc_cidr
+  environment = var.environment
+  project  = var.project
+  # TODO pass the correct AZ subnets for security groups, 
+  # which we generated in the VPC module since they are always different length in each region, 
+  # allow only private subnet access to the database LB. 
+}
